9112cefb2f386cd8d9d2c362f4eb41f027712f29
laurencegreg <32897863+laurencegreg@users.noreply.github.com>
2020-12-08 12:53:31 +0100

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,18 @@
+f = open("input")
+lines = f.readlines()
+f.close()
+acc = 0
+i=0
+while "end" not in lines[i] :
+    if "nop" in lines[i] :
+        lines[i]="end"
+        i+=1
+    if "acc" in lines[i] :
+        acc += int(lines[i].split(" ")[1])
+        lines[i]="end"
+        i+=1
+    if "jmp" in lines[i] :
+        jump = int(lines[i].split(" ")[1])
+        lines[i]="end"
+        i+=jump
+print(acc)
