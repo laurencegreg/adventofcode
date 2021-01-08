7fbc1d2a7823e25a15286f2e9835ba865b79c248
Grégoire Laurence <laurencegreg@gmail.com>
2021-01-08 12:38:24 +0100

    22_nolog

--- 2020/22/22_2.py
+++ 2020/22/22_2.py
@@ -20,50 +20,54 @@ def recGame(player1,player2,tab) :
     rec = False
     tabSpaces = "".join(["   " for i in range(0,tab)])
     while len(player1) != 0 and len(player2) != 0 and not rec :
-        print(tabSpaces+"--------------")
-        print(tabSpaces+"Player 1's deck:",player1)
-        print(tabSpaces+"Player 2's deck:",player2)
+#        print(tabSpaces+"--------------")
+#        print(tabSpaces+"Player 1's deck:",player1)
+#        print(tabSpaces+"Player 2's deck:",player2)
         card1 = player1.pop(0)
         card2 = player2.pop(0)
-        print(tabSpaces+"Player 1 plays:",card1)
-        print(tabSpaces+"Player 2 plays:",card2)
+#        print(tabSpaces+"Player 1 plays:",card1)
+#        print(tabSpaces+"Player 2 plays:",card2)
         if len(player1)>=card1 and len(player2)>=card2 :
             (res,_,_) =recGame(player1[:card1],player2[:card2],tab+1)
             if res :
-                print(tabSpaces+"Player 1 wins Game")
+#                print(tabSpaces+"Player 1 wins Game")
                 player1.append(card1)
                 player1.append(card2)
             else :
-                print(tabSpaces+"Player 2 wins Game")
+#                print(tabSpaces+"Player 2 wins Game")
                 player2.append(card2)
                 player2.append(card1)
         else :
             if card1 > card2 :
-                print(tabSpaces+"Player 1 wins")
+#                print(tabSpaces+"Player 1 wins")
                 player1.append(card1)
                 player1.append(card2)
             else :
-                print(tabSpaces+"Player 2 wins")
+#                print(tabSpaces+"Player 2 wins")
                 player2.append(card2)
                 player2.append(card1)
         state = ",".join(list(map(str,player1)))+"|"+",".join(list(map(str,player2)))
         if state in states :
-            print(tabSpaces+"--------------")
-            print(tabSpaces+"already seen state "+state)
-            print(tabSpaces+"end of subGame : player 1 win")
+#            print(tabSpaces+"--------------")
+#            print(tabSpaces+"already seen state "+state)
+#            print(tabSpaces+"end of subGame : player 1 win")
             rec = True
         else :
             states.add(state)
-        print(tabSpaces+"--------------")
+#        print(tabSpaces+"--------------")
 
     return (rec or len(player1)>0,player1,player2)
 
 (res,player1,player2) = recGame(player1,player2,0)
 
-
+player = []
+if res :
+    player = player1
+else :
+    player = player2
 counter = 0
-for i in range(0,len(player2)) :
-    counter += (i+1)*player2[len(player2)-i-1]
-    print((i+1),"*",player2[len(player2)-i-1])
+for i in range(0,len(player)) :
+    counter += (i+1)*player[len(player)-i-1]
+    print((i+1),"*",player[len(player)-i-1])
 
 print(counter)
