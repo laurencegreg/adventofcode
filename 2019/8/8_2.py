65ecd86a22eb4a1fd7afc91d233de8a0e5188d51
Grégoire Laurence <laurencegreg@gmail.com>
2021-01-12 18:34:52 +0100

    2019_2

--- 2019/8/8_2.py
+++ 2019/8/8_2.py
@@ -12,6 +12,10 @@ def addLayer(image,layer):
             layerPixel = layer[i*25+j]
             if layerPixel in ["0","1"]:
                 newImage[i][j]=layerPixel
+    for line in list(map(lambda x : "".join(x).replace("0"," ").replace("1","▮").replace("2"," "),newImage)) :
+        print(line)
+    print("")
+    print("")
     return newImage
 
 
@@ -19,6 +23,4 @@ image=[["2" for x in range(0,25)] for y in range(0,6)]
 for i in reversed(range(0,len(data)//(25*6))):
     image=addLayer(image,data[i*(25*6):(i+1)*(25*6)])
 
-for line in list(map(lambda x : "".join(x).replace("0"," "),image)) :
-    print(line)
 
