952e05012ec4e3eb436f04aeb435da57d5691186
Grégoire Laurence <laurencegreg@gmail.com>
2021-12-01 18:18:53 +0100

    timer

--- 2021/1/1_2.go
+++ 2021/1/1_2.go
@@ -1,39 +1,45 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "os"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
 	"strconv"
+	"time"
 )
 
+func timeTrack(start time.Time) {
+	elapsed := time.Since(start)
+	fmt.Println("It took", elapsed)
+}
 func main() {
-    file, err := os.Open("input")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+	defer timeTrack(time.Now())
+	file, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 	var a [3]int
 	count := 0
 	it := 0
 	newVal := 0
-    for scanner.Scan() {
-        newVal, err = strconv.Atoi(scanner.Text())
-		if (it>=2){
-			if (it>2 && a[it%3]<newVal){
+	for scanner.Scan() {
+		newVal, err = strconv.Atoi(scanner.Text())
+		if it >= 2 {
+			if it > 2 && a[it%3] < newVal {
 				count++
 			}
 		}
 		a[it%3] = newVal
 		it++
-    }
+	}
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println(count)
 }
